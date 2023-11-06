b818934ef690418823d9f0c64ed2645231e78b10
PC <black-angel-eremin@yandex.ru>
2023-11-06 18:43:13 +0300

    Мой коммит

---
+++ src/Transport/Transport.java
@@ -0,0 +1,35 @@
+package Transport;
+
+import Service.Servicable;
+
+public abstract class Transport implements Servicable {
+
+    private final String modelName;
+
+    private final int wheelsCount;
+
+    public Transport(String modelName, int wheelsCount) {
+        this.modelName = modelName;
+        this.wheelsCount = wheelsCount;
+    }
+
+    public void updateTyre() {
+        System.out.println("Меняем покрышку");
+    }
+
+    public String getModelName() {
+        return modelName;
+    }
+
+    public int getWheelsCount() {
+        return wheelsCount;
+    }
+
+    @Override
+    public void check() {
+        for (int i = 0; i < wheelsCount; i++) {
+            updateTyre();
+
+        }
+    }
+}
